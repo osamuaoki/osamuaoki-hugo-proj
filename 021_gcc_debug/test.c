c45fac8e84f21876f65af18b2c0fa770fe1617c7
Osamu Aoki <osamu@debian.org>
2024-06-08 16:31:17 +0900

    Fix error
    Signed-off-by: Osamu Aoki <osamu@debian.org>

--- 021_gcc_debug/test.c
+++ 021_gcc_debug/test.c
@@ -4,11 +4,11 @@ int main() {
   int i, final, factorial = 1;
   printf("Enter the number: ");
   if (scanf("%d", &final)) {
-    exit(1);
-  } else {
     for (i = 1; i <= final; i++)
       factorial = factorial * i;
     printf("The factorial of %d is %d\n", final, factorial);
     exit(0);
+  } else {
+    exit(1);
   }
 }
